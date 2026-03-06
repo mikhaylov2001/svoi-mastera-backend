3e03fc4f3ea0a1fe502799bd6fd7d7e7f58249a5
dimon9995 <mikhailov1500@mail.ru>
2026-03-06 19:07:03 +0300

    push

---
+++ src/main/java/ru/svoi/mastera/backend/SvoiMasteraBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.svoi.mastera.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvoiMasteraBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvoiMasteraBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/svoi/mastera/backend/SvoiMasteraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.svoi.mastera.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvoiMasteraBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
